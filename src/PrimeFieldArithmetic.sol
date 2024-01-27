4213496ca63fd2dc9c98f8a01cd5780602a099e0
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-01-27 23:11:06 +0100

    Create PrimeFieldArithmetic.sol

---
+++ src/PrimeFieldArithmetic.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/// @title PrimeFieldArithmetic
+/// @notice Library for performing arithmetic operations in a prime field
+/// @author Diego (0xfuturistic@pm.me)
+library PrimeFieldArithmetic {
+    /// @notice The prime number from the secp256k1 curve is used.
+    uint256 constant PRIME = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
+
+}
