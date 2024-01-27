f9f4efb4450f84627bdfd7b5c0fe3c8db8bc4c3b
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-01-27 23:12:18 +0100

    Create add function

--- src/PrimeFieldArithmetic.sol
+++ src/PrimeFieldArithmetic.sol
@@ -12,4 +12,12 @@ library PrimeFieldArithmetic {
         require(value < PRIME, "value must be in finite field");
         _;
     }
+
+    /// @notice Adds two numbers in the prime field
+    /// @param a The first number to add
+    /// @param b The second number to add
+    /// @return The sum of a and b in the prime field
+    function add(uint256 a, uint256 b) internal pure mustBeInField(a) mustBeInField(b) returns (uint256) {
+        return addmod(a, b, PRIME);
+    }
 }
